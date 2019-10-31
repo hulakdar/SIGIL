c4952fed8517fb8bd2bb4e880486ca3c110eaf03
Semen KAMOZA <skamoza@e1r7p14.unit.ua>
2019-10-31 16:03:08 +0200

    flip sigil

--- src/util/images.c
+++ src/util/images.c
@@ -20,8 +20,6 @@ GLuint loadOpenGLTexture(const char *filename)
     int width;
     int height;
     unsigned char *data;
-    unsigned char *temp;
-    int i;
     GLuint result = 0;
 
 	// much faster than using SOIL_load_OGL_texture(), plus this doesn't use any deprecated functionality that is illegal in 3.2 core contexts
@@ -31,20 +29,24 @@ GLuint loadOpenGLTexture(const char *filename)
 	if(data)
 	{
 		// the pixel data is flipped vertically, so we need to flip it back; this uses an in-place reversal
-		temp = (unsigned char*)malloc(sizeof(unsigned char) * width * 4);					// enough space for one row of RGBA pixels
-		for(i = 0; i < height / 2; i ++)
-		{
-			memcpy(temp, &data[i * width * 4], (width * 4));								// copy row into temp array
-			memcpy(&data[i * width * 4], &data[(height - i - 1) * width * 4], (width * 4));	// copy other side of array into this row
-			memcpy(&data[(height - i - 1) * width * 4], temp, (width * 4));					// copy temp into other side of array
-		}
-		free(temp);
+		// temp = (unsigned char*)malloc(sizeof(unsigned char) * width * 4);					// enough space for one row of RGBA pixels
+		// for(i = 0; i < height / 2; i ++)
+		// {
+		// 	memcpy(temp, &data[i * width * 4], (width * 4));								// copy row into temp array
+		// 	memcpy(&data[i * width * 4], &data[(height - i - 1) * width * 4], (width * 4));	// copy other side of array into this row
+		// 	memcpy(&data[(height - i - 1) * width * 4], temp, (width * 4));					// copy temp into other side of array
+		// }
+		// free(temp);
 
 		// we can generate a texture object since we had a successful load
 		glGenTextures(1, &result);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, result);
+#if __APPLE__
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, data);
+#else
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+#endif
 
 		// we want textures to be wrappable
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
