a0aee417041bcda0f261c849290068da962670dd
Semen KAMOZA <skamoza@e1r7p14.unit.ua>
2019-10-31 21:14:28 +0200

    color

--- src/util/images.c
+++ src/util/images.c
@@ -42,11 +42,7 @@ GLuint loadOpenGLTexture(const char *filename)
 		glGenTextures(1, &result);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, result);
-#if __APPLE__
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, data);
-#else
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-#endif
 
 		// we want textures to be wrappable
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
